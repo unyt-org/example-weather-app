6ff6ec3bba4570f60d21dcf86a0ee6106d76481a
Jonas Strehle <jonas@strehles.info>
2023-10-25 08:19:12 +0200

    Fix

--- backend/entrypoint.tsx
+++ backend/entrypoint.tsx
@@ -40,6 +40,7 @@ const API_KEY = Datex.Runtime.ENV.API_TOKEN ?? "<YOUR_API_TOKEN>";
 logger.info("Using API key", API_KEY);
 const getWeather = async (location: string) => {
 	const result: any = await datex.get(`http://api.weatherapi.com/v1/forecast.json?key=${encodeURIComponent(API_KEY)}&q=${encodeURIComponent(location)}&days=4&aqi=yes&alerts=no`)
+	logger.debug(result);
 	const current = result.current;
 	const today = result.forecast.forecastday[0];
 	const weather: Weather = {
@@ -50,7 +51,8 @@ const getWeather = async (location: string) => {
 		current: {
 			uv: current.uv,
 			isDay: current.is_day,
-			air: current.air_quality["us-epa-index"] - 1,
+			air: current.air_quality ? 
+				current.air_quality?.["us-epa-index"] - 1 : 0,
 			text: current.condition.text,
 			icon: current.condition.icon.match(/[^\/]+\/[^\/]+\.png/i).at(0),
 			temp: current.temp_c,
@@ -66,7 +68,8 @@ const getWeather = async (location: string) => {
 				temp: day.day.avgtemp_c,
 				tempMax: day.day.maxtemp_c,
 				tempMin: day.day.mintemp_c,
-				air: day.day.air_quality["us-epa-index"] - 1
+				air: day.day.air_quality ? 
+					day.day.air_quality?.["us-epa-index"] - 1 : 0
 			}
 		})
 	}
