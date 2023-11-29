055af3874ce26dafbd23e2e4aa861b6cffa386dd
Jonas Strehle <jonas@unyt.org>
2023-11-29 14:30:39 +0100

    Fix theme

--- common/theme.tsx
+++ common/theme.tsx
@@ -1,14 +1,22 @@
 import { UIX } from "uix";
-import { defaultThemes } from "uix/base/themes.ts";
+import { uixLightPlain } from "uix/themes/uix-light-plain.ts";
+import { uixDarkPlain } from "uix/themes/uix-dark-plain.ts";
 
-UIX.Theme.registerTheme(UIX.Theme.extend(defaultThemes.dark, {
-	mode: undefined,
-	name: "weather-theme",
+UIX.Theme.registerTheme(UIX.Theme.extend(uixDarkPlain, {
+	name: "unyt-dark",
 	values: {
 		"accent": "white",
 		"color-1": "#ff8080",
 		"color-2": "#554afa"
-	},
-	stylesheets: ["./weather-theme.scss"]
+	}
 }));
-UIX.Theme.useThemes("weather-theme");
+UIX.Theme.registerTheme(UIX.Theme.extend(uixLightPlain, {
+	name: "unyt-light",
+	values: {
+		"accent": "white",
+		"color-1": "#ff8080",
+		"color-2": "#554afa"
+	}
+}));
+
+UIX.Theme.useThemes("unyt-light", "unyt-dark");
